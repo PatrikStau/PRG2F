90310e8dcf25b8aee9a4792ee56ba880be9b5973
Patrik Stauber <patrik.stauber@infis.cz>
2024-10-03 14:41:56 +0200

    second commit

---
+++ src/Variables.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Zadej své jméno");
+        String jmeno = sc.next();
+
+        System.out.println();
+        int a = sc.nextInt();
+
+        System.out.println();
+        double c = sc.nextDouble();
+
+        System.out.println();
+        char d = sc.next().charAt(0);
+
+        System.out.println();
+        boolean b;
+    };
+}
+
