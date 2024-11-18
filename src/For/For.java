458be2f74ae5ed01fc997e57d4cd91ac3ae6e57f
PatrikStau <patrik.stauber@infis.cz>
2024-11-18 13:19:31 +0100

    commit

--- src/For/For.java
+++ src/For/For.java
@@ -23,9 +23,8 @@ public class For {
         /* vypsat malou násobilku */
 
 
-
         for (int i = 1; i <= 10; i++) {
-            for (int j = 0; j <= 10 ; j++) {
+            for (int j = 0; j <= 10; j++) {
                 System.out.print(i * j + " ");
             }
             System.out.println();
