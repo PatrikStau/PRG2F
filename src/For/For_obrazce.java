36dbe2e67a8a07a22dde9b7f43594660c8c6c485
PatrikStau <patrik.stauber@infis.cz>
2024-11-18 13:20:28 +0100

    commit

--- src/For/For_obrazce.java
+++ src/For/For_obrazce.java
@@ -17,10 +17,11 @@ public class For_obrazce {
             System.out.println();
 
         }
+        System.out.println();
 
         for (int radek = 0; radek < velikostMrizky; radek++) {
             for (int sloupec = 0; sloupec < velikostMrizky; sloupec++) {
-                if (radek == 0 || radek == velikostMrizky - 1 || radek == (velikostMrizky - 1) - sloupec) {
+                if (radek == 0 || radek == velikostMrizky - 1 || sloupec == 0 || sloupec == velikostMrizky - 1) {
                     System.out.print(" X ");
                 } else {
                     System.out.print(" ■ ");
