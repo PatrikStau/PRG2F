4d52d9b8f94e84e696e2c3bba256d9854a50cfaa
PatrikStau <patrik.stauber@infis.cz>
2024-12-01 21:56:59 +0100

    second commit

---
+++ src/Úkol_string.java
@@ -0,0 +1,22 @@
+import java.util.AbstractQueue;
+import java.util.Scanner;
+
+public class Úkol_string {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Zadej jméno.");
+        String jmeno = sc.next();
+        System.out.println("Zadej příjmení.");
+        String prijmeni = sc.next();
+        String jmenoPozpatku = "";
+        String prijmeniPozpatku = "";
+
+        for (int i = jmeno.length() - 1; i >= 0 ; i--) {
+            jmenoPozpatku = jmenoPozpatku + jmeno.charAt(i);
+        }
+        for (int i = prijmeni.length() - 1; i >= 0; i--) {
+            prijmeniPozpatku = prijmeniPozpatku + prijmeni.charAt(i);
+        }
+        System.out.println(jmenoPozpatku.toUpperCase() + " " + prijmeniPozpatku.toUpperCase());
+    }
+}
