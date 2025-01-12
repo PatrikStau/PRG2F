1a3b2b0bad51df55c23018a056cf31727eccc963
PatrikStau <patrik.stauber@infis.cz>
2025-01-12 17:53:34 +0100

    second commit

---
+++ src/Ukol_metody.java
@@ -0,0 +1,30 @@
+public class Ukol_metody {
+    static void validatePassword(String password){
+        // heslo musí být dlouhé minimálně 8 znaků
+        if (password.length() < 8){
+            System.out.println("Heslo nesplňuje jednu z podmínek: \"moc krátké\"");
+            return;
+        }
+        // heslo nesmí obsahovat slovo heslo
+        else if (password.toLowerCase().contains("heslo")){
+            System.out.println("Heslo nesplňuje jednu z podmínek: \"obsahuje zakázané slovo\"");
+            return;
+        }
+        // heslo musí být dlouhé maximálně 25 znaků
+        else if (password.length() > 25){
+            System.out.println("Heslo nesplňuje jednu z podmínek: \"moc dlouhé\"");
+            return;
+        }
+        else {
+            System.out.println("Heslo je v pořádku.");
+        }
+    }
+
+    public static void main(String[] args) {
+        validatePassword("Arnost1");
+        validatePassword("heslo123");
+        validatePassword("ArnostmilujeOtrokovice2025");
+        validatePassword("Arnostek1");
+
+    }
+}
