aa7dbda76411c6c3939303b77a7ac0f7ed966cfc
patrik.stauber <patrik.stauber@infis.cz>
2025-03-21 10:35:19 +0100

    commit

---
+++ src/arraylist_tridy.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class arraylist_tridy {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<ArrayList<String>> seznamTrid = new ArrayList<>();
+
+        System.out.print("Zadej počet tříd.");
+        int pocetTrid = sc.nextInt();
+
+        for (int i = 0; i < pocetTrid; i++) {
+            seznamTrid.add(new ArrayList<>());
+
+            System.out.println("Zadej počet žáků v " + (i + 1) + ". třídě");
+            int pocetZaku = sc.nextInt();
+
+            System.out.println("Zadej jméno žáka.");
+            for (int j = 0; j < pocetZaku; j++) {
+                String zak = sc.next();
+                seznamTrid.get(i).add(zak);
+            }
+        }
+        for (int i = 0; i < pocetTrid; i++) {
+            System.out.println((i + 1) + ". třída: " + seznamTrid.get(i));
+        }
+
+    }
+}
